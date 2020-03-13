6d3481b0472fcf5feb4fa792ade2e5a6c537f93d
Erica Zavaleta <micfeedbak@gmail.com>
2020-03-12 19:25:24 -0500

    Updated everything to Circuit Python 5.0.0 and set the main code.py to a base hello world to build off of

--- code.py
+++ code.py
@@ -1,35 +1,16 @@
+import sys
 import time
 import board
 from adafruit_pyportal import PyPortal
 
-# Set up where we'll be fetching data from
-DATA_SOURCE = "https://www.adafruit.com/api/quotes.php"
-QUOTE_LOCATION = [0, 'text']
-AUTHOR_LOCATION = [0, 'author']
+cwd = ("/"+__file__).rsplit('/', 1)[0] # the current working directory (where this file is)
 
-# the current working directory (where this file is)
-cwd = ("/"+__file__).rsplit('/', 1)[0]
-pyportal = PyPortal(url=DATA_SOURCE,
-                    json_path=(QUOTE_LOCATION, AUTHOR_LOCATION),
-                    status_neopixel=board.NEOPIXEL,
-                    default_bg=cwd+"/quote_background.bmp",
-                    text_font=cwd+"/fonts/Arial-ItalicMT-17.bdf",
-                    text_position=((20, 120),  # quote location
-                                   (5, 210)), # author location
-                    text_color=(0xFFFFFF,  # quote text color
-                                0x8080FF), # author text color
-                    text_wrap=(35, # characters to wrap for quote
-                               0), # no wrap for author
-                    text_maxlen=(180, 30), # max text size for quote & author
-                   )
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
 
-# speed up projects with lots of text by preloading the font!
-pyportal.preload_font()
+print("Hello Pyportal")
 
-while True:
-    try:
-        value = pyportal.fetch()
-        print("Response is", value)
-    except RuntimeError as e:
-        print("Some error occured, retrying! -", e)
-    time.sleep(60)
