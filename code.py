3403fdef32fee6ca3ef1b1a693122c79c397c1af
Erica Zavaleta <micfeedbak@gmail.com>
2020-03-13 23:36:29 -0500

    Added polling of time from a restful API server to the clock gets updated

--- code.py
+++ code.py
@@ -2,12 +2,16 @@ import sys
 import time
 import board
 import busio
+import neopixel
+import rtc # For interfacing with the Real Time Clock
 import terminalio  # For using the terminal basic font
-import displayio
+import displayio # Library for writing text / graphics to the screen
 import adafruit_adt7410  # For polling the onboard 7410 Temp Sensor
+from digitalio import DigitalInOut # Enabling DigitalIO so we can talk to the ESP32 Wifi chip.
 #from adafruit_pyportal import PyPortal
 from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text import label
+from adafruit_esp32spi import adafruit_esp32spi, adafruit_esp32spi_wifimanager
 
 cwd = ("/"+__file__).rsplit('/', 1)[0] # the current working directory (where this file is)
 
@@ -18,23 +22,62 @@ except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-print("Hello Pyportal")
+# PyPortal ESP32 Setup
+esp32_cs = DigitalInOut(board.ESP_CS)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
+wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light)
 
 # Initialize the ADT7410 Temp Sensor
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 adt = adafruit_adt7410.ADT7410(i2c_bus, address=0x48)
 adt.high_resolution = True
 
-temperature = adt.temperature
-temperature = (temperature * 1.8 +32)
-print(temperature)
+# Initialize the Real Time Clock
+the_rtc = rtc.RTC()
 
+# Initialize the Display
 display = board.DISPLAY
+
+# Set some Global variables
+time_api = "http://worldtimeapi.org/api/ip"
 
 font = bitmap_font.load_font("/fonts/Arial-ItalicMT-17.bdf")
 color = 0x0000FF
 
 print("Initializing")
+wifi.connect()
+
+# Get the time from the time api server. 
+response = None
+while True:
+    try:
+        print("Fetching json from", time_api)
+        response = wifi.get(time_api)
+        break
+    except (ValueError, RuntimeError) as e:
+        print("Failed to get data, retrying\n", e)
+        continue
+
+# Parse the time out of the API Response
+json = response.json()
+current_time = json['datetime']
+the_date, the_time = current_time.split('T')
+year, month, mday = [int(x) for x in the_date.split('-')]
+the_time = the_time.split('.')[0]
+hours, minutes, seconds = [int(x) for x in the_time.split(':')]
+year_day = json['day_of_year']
+week_day = json['day_of_week']
+is_dst = json['dst']
+
+# Update the RTC
+now = time.struct_time((year, month, mday, hours, minutes, seconds, week_day, year_day, is_dst))
+print(now)
+the_rtc.datetime = now
+
 time.sleep(10)
 
 while True:
