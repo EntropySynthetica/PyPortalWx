42e6c533b0a47c6df0d7aedd90e56646dfd96699
Erica Zavaleta <micfeedbak@gmail.com>
2020-03-31 18:01:59 -0500

    Added an example of the secrets.py file which stores wifi details and api keys

---
+++ secrets_example.py
@@ -0,0 +1,11 @@
+# This is where you store your creds to your wifi, and your API key for Openweathermap.org.
+# Rename this file to secrets.py before use and enter your wifi / api details. 
+
+secrets = {
+    'ssid' : 'my_wifi',             # Wifi Name to connect to
+    'password' : 'mypassword123',         # Wifi Password
+    'timezone' : "America/Chicago",    # timezone name
+    'time_api' : 'http://worldtimeapi.org/api/timezone/america/chicago',  # API location to get time from. 
+    'owm_apikey' : "xxxxxxxx",  # Open Weather Map API Key
+    'owm_cityid' : "4887398",  # Open Weather Map city ID. 
+    }
